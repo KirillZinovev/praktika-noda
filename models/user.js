f83277fa174510535e9eed78ca399d87dd0b3de6
KirillZinovev <kirillzinovev23@gmail.com>
2024-01-11 15:31:39 +0300

    commit g

--- models/user.js
+++ models/user.js
@@ -1,40 +1,57 @@
 const sqlite3 = require("sqlite3").verbose();
-const bcrypt = require("bcrypt");
-const res = require("express/lib/response");
+const res = require("express/lib/response"); 
 const db = new sqlite3.Database("test.sqlite");
 
+// Создание таблицы, если она не существует
 const sql =
   "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT NOT NULL, email TEXT NOT NULL, password TEXT NOT NULL, age INT NOT NULL)";
-
 db.run(sql);
 
 class User {
   constructor() {}
+
+  // Создание нового пользователя
   static async create(dataForm, cb) {
     try {
-      // const salt = await bcrypt.genSalt(10);
+  // const salt = await bcrypt.genSalt(10);
       // const hash = await bcrypt.hash(dataForm.password, salt);
 
-      const sql1 =
-        "INSERT INTO users (name, email, password, age) VALUES (?, ?, ?, ?)";
-      db.run(sql1, dataForm.name, dataForm.email, dataForm.password, dataForm.age, cb);
+      if (dataForm.password.length < 7) {
+        throw new Error('Пароль должен содержать не менее 7 символов');
+      }
+  
+      const registerPassword = /^(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=]).{7,}$/;
+      if (!registerPassword.test(dataForm.password)) {
+        throw new Error('Пароль должен содержать одну букву, одну цифру, одну заглавную букву и один специальный символ');
+      }
+  
+      const registerEmail = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/g;
+      if (!registerEmail.test(dataForm.email)) {
+        throw new Error('Введите почту корректно)');
+      }
+
+      const sql = 'INSERT INTO users (name, email, password, age) VALUES (?, ?, ?, ?)';
+      db.run(sql, [dataForm.name, dataForm.email, dataForm.password, dataForm.age], cb);
     } catch (error) {
-      if (error) return next(error);
+      return cb(error);
     }
   }
 
+  // Поиск пользователя по email
   static findByEmail(email, cb) {
     db.get("SELECT * FROM users WHERE email = ?", email, cb);
   }
+
+  // Аутентификация пользователя
   static authentificate(dataForm, cb) {
     User.findByEmail(dataForm.email, (error, user) => {
       if (error) return cb(error);
-      if (!user) return cb();
-      if (dataForm.password === user.password) {
-        return cb(null, user);
-      } else {
-        return cb();
+      if (!user) return cb(); // Пользователь не найден
+      if (dataForm.password !== user.password) {
+        return cb(); // Неверный пароль
       }
+  
+      cb(null, user); // Аутентификация успешна
     });
   }
   // static authentificate(dataForm, cb) {
